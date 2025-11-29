ba5d98e14b5529d3810586fed38c5be9bc2dcb68
Dhinesh Palanisamy <84257275+dhineshaps@users.noreply.github.com>
2025-11-29 14:28:25 +0530

    Enhance portfolio analyzer with user input and validation

--- app.py
+++ app.py
@@ -1,18 +1,53 @@
 import streamlit as st
 import asyncio
+import pandas as pd
 from agents.portfolio_runner import run_query
 from utils.portfolio_report import portfolio_report
 
-st.title("📊 FetQuest Portfolio Analyzer")
+st.markdown("<h1 style='text-align: center; color: #DAA520;font-size: 35px'>The FET Quest - OneView</h1>", unsafe_allow_html=True)
 
-portfolio = {"HDFCBANK": 50000, "TCS": 30000, "HDFC": 20000}
-target = {"HDFCBANK": 0.4, "TCS": 0.3, "HDFC": 0.3}
+st.title("📊 AI Stock Portfolio Analyzer")
 
+st.write("Enter your stocks, current value, and target weights (%).")
+
+
+default_data = {
+    "Asset": ["HDFC","ITC"],
+    "Current Value": [50000,4000],
+    "Target %": [40,60]
+}
+
+df = st.data_editor(
+    pd.DataFrame(default_data),
+    num_rows="dynamic",
+    key="portfolio_editor"
+)
+
+total_weight = df["Target %"].sum()
+
+if total_weight != 100:
+    st.warning(f"⚠️ Target weights must add up to 100%. Current total = {total_weight}%", icon="⚠️")
+else:
+    st.success("✅ Target weights sum to 100%.")
+
+#converting to dict
+portfolio = dict(zip(df["Asset"], df["Current Value"]))
+print(portfolio)
+
+#calculating percentage from real value
+target = {row["Asset"]: row["Target %"] / 100 for _, row in df.iterrows()}
+print(target)
+
+
+#ADK Analaysis
 if st.button("Analyze Portfolio"):
-    query = f"Portfolio={portfolio}, Target={target}. Analyze and rebalance."
+    if total_weight != 100:
+        st.error("❌ Cannot analyze: Target % must equal 100%.")
+    else:
+        query = f"Portfolio={portfolio}, Target={target}. Analyze and rebalance."
 
-    with st.spinner("Analyzing…"):
-        result = asyncio.run(run_query(query))
+        with st.spinner("Analyzing…"):
+            result = asyncio.run(run_query(query))
 
-    st.subheader("Raw Final Output")
-    st.code(result)
+        st.subheader("Raw Final Output")
+        st.code(result)
